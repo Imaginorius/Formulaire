e06748270bb6d282bec1213dc2599ea712616219
JeanOryon <48481925+JeanOryon@users.noreply.github.com>
2019-11-13 23:14:20 +0100

    Add files via upload

---
+++ Formulaire/js/formulaire.js
@@ -0,0 +1,35 @@
+var nbcarac = document.getElementById('carac');
+var dispcarac= document.getElementById('dispnbchar');
+
+/* changer de page */
+function changepage() {
+    
+   /* var count= nbcarac.value.length;
+   if(count>=10) {*/
+     document.getElementById('diaporama').style.marginLeft="-100%";
+
+   /*}*/
+}
+
+
+/* Afficher le nombre de caractère*/
+function countchar() {
+    var count= nbcarac.value.length;
+            dispcarac.innerHTML=count;
+
+    if(count < 10) {
+        dispcarac.innerHTML="Veuillez saisir plus 10 caractères";
+    }
+
+    else{
+        dispcarac.innerHTML=count;
+
+    }
+
+
+}
+
+/* passer page*/
+function Pchange() {
+    document.getElementById('diapo').style.marginLeft="0%";
+}
